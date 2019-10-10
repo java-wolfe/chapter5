8a88f48564d76b9ffd05b0c8920b2ddc3e7387d2
DakotaWolfe <54542632+DakotaWolfe@users.noreply.github.com>
2019-10-10 10:33:45 -0500

    commit

--- src/Assignment/AscendingAndDescending.java
+++ src/Assignment/AscendingAndDescending.java
@@ -1,8 +1,56 @@
 package Assignment;
 
+import java.util.Scanner;
+
 public class AscendingAndDescending {
 
     public static void main(String[] args){
-        
+        Scanner input = new Scanner(System.in);
+        int entry1;
+        int entry2;
+        int entry3;
+        String result = null;
+        String result1 = null;
+
+        System.out.println("Enter a number >>>");
+        entry1 = input.nextInt();
+
+        System.out.println("Enter a second number >>>");
+        entry2 = input.nextInt();
+
+        System.out.println("Enter a third number >>>");
+        entry3 = input.nextInt();
+
+        if (entry1 > entry2 && entry1 > entry3){
+            if (entry2 > entry3){
+                result = "Ascending Order " + entry1 + " , " + entry2 + " , " + entry3;
+                result1 = "Descending Order " + entry3 + " , " + entry2 + " , " + entry1;
+            }
+            else {
+                result = "Ascending Order " + entry1 + " , " + entry3 + " , " + entry2;
+                result1 = "Descending Order " + entry3 + " , " + entry2 + " , " + entry1;
+            }
+        }
+        else if (entry2 > entry1 && entry2 > entry3){
+            if (entry1 > entry3){
+                result = "Ascending Order " + entry2 + " , " + entry1 + " , " + entry3;
+                result1 = "Descending Order " + entry3 + " , " + entry1 + " , " + entry2;
+            }
+            else {
+                result = "Ascending Order " + entry2 + " , " + entry3 + " , " + entry1;
+                result1 = "Descending Order " + entry1 + " , " + entry3 + " , " + entry2;
+            }
+        }
+        else if (entry3 > entry1 && entry3 > entry2){
+            if (entry1 > entry2){
+                result = "Ascending Order " + entry3 + " , " + entry1 + " , " + entry2;
+                result1 = "Descending Order " + entry2 + " , " + entry1 + " , " + entry3;
+            }
+            else {
+                result = "Ascending Order " + entry3 + " , " + entry2 + " , " + entry1;
+                result1 = "Descending Order " + entry1 + " , " + entry2 + " , " + entry3;
+            }
+        }
+        System.out.println(result + "\n" + result1);
     }
 }
